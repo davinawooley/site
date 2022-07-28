a752dd30547d063625a242d6754ad029b141faad
deeWooley <wooleymediallc@gmail.com>
2022-07-28 18:18:49 -0400

    vite update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist/',
+
   build: {
     chunkSizeWarningLimit:1500,
     rollupOptions: {
