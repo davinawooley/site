0f339bf293b5026e197e0cd19d861b5619cc3e59
deeWooley <wooleymediallc@gmail.com>
2022-07-29 09:09:35 -0400

    toml update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,46 @@
+// import { defineConfig } from 'vite'
+// export default defineConfig({
+  // build: {
+    // chunkSizeWarningLimit:1500,
+    // rollupOptions: {
+        // output:{
+//             manualChunks(id) {
+//               if (id.includes('node_modules')) {
+                
+//                   return id.toString().split('node_modules/')[1].split('/')[0].toString();
+//               }
+//           }
+//         }
+//     }
+//   }
+// })
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
 export default defineConfig({
   build: {
     chunkSizeWarningLimit:1500,
+    // lib: {
+      // entry: resolve(__dirname, 'lib/main.js'),
+      // name: 'MyLib',
+      // the proper extensions will be added
+      // fileName: 'my-lib'
+    // },
     rollupOptions: {
-        output:{
-            manualChunks(id) {
-              if (id.includes('node_modules')) {
-                
-                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
-              }
+
+      external: ['vue'],
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
+        // globals: {
+        //   vue: 'Vue'
+        // }
+      }
     }
   }
 })
