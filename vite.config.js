c07246d8e5208ebc6ff0f41fc92f7d61d4f83d0d
deeWooley <wooleymediallc@gmail.com>
2022-07-29 13:41:15 -0400

    toml update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,3 @@
-// import { defineConfig } from 'vite'
-// export default defineConfig({
-  // build: {
-    // chunkSizeWarningLimit:1500,
-    // rollupOptions: {
-        // output:{
-//             manualChunks(id) {
-//               if (id.includes('node_modules')) {
-                
-//                   return id.toString().split('node_modules/')[1].split('/')[0].toString();
-//               }
-//           }
-//         }
-//     }
-//   }
-// })
-
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
