bf481c74f06e79bafea32b1adfec39a3a0d2653c
deeWooley <wooleymediallc@gmail.com>
2022-07-28 23:51:35 -0400

    three js

--- threejsscript.js
+++ threejsscript.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from '/node_modules/three/build/three.module.js'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three-orbitcontrols'
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { gsap } from 'gsap'
 
